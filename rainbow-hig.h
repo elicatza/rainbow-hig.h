dff77272ab3d963e2a6f836adb1181ac85b9f101
elicatza <eliza.cl@protonmail.com>
2023-07-29 08:27:11 +0200

    stable before refactor

--- rainbow-hig.h
+++ rainbow-hig.h
@@ -36,43 +36,85 @@
 #include <stdbool.h>
 
 typedef enum {
-    RH_ARG_REQUIRED,
-    RH_ARG_OPTIONAL,
-} RHArgType;
+    RH_ARG_REQUIRED = 1<<1,
+    RH_ARG_OPTIONAL = 1<<2,
+} RHFlagType;
 
 typedef struct {
     char *longarg;
-    char *shortarg;
-    RHArgType argtype;
+    char shortarg;
+    RHFlagType argtype;
     void (*parse)(int *argc, char ***argv, void **var);
     void **var;
-} RHArgs;
+} RHFlag;
 
-static char *rh_args_shift(int *argc, char ***argv);
-extern void rh_args_parse(int argc, char **argv, RHArgs *args);
+typedef struct {
+    int argc;
+    char **argv;
+    void *var;
+    unsigned int flag_type;
+    char *optval;
+} RHOpt;
+
+/*
+typedef struct {
+    char *help;
+    char *usage;
+} RHFoo;
+*/
+
+#define RHARG_NULL NULL, 0, 0, NULL, NULL
+
+static inline bool rh__arg_is_null(RHFlag arg);
+
+extern char *rh_args_shift(int *argc, char ***argv);
+extern void rh_args_parse(int argc, char **argv, RHFlag *args);
 
 extern void rh_parser_str(int *argc, char ***argv, void **var);
 extern void rh_parser_bool(int *argc, char ***argv, void **var);
+extern void rh_action_help(int *argc, char ***argv, void **var);
+
 
 #ifdef RH_IMPLEMENTATION
 
 #endif // RH_IMPLEMENTATION
 
-extern void rh_args_parse(int argc, char **argv, RHArgs *args)
+extern void rh_args_parse(int argc, char **argv, RHFlag *args)
 {
     // TODO: ensure unique flags
-    printf("size out: %zu\n", sizeof *args);
-    RHArgs *inc_args;
+    // TODO: check required flags
+    size_t i;
+    char *ip;
     do {
         char *arg = rh_args_shift(&argc, &argv);
-        for (inc_args = args; inc_args->longarg != 0; ++inc_args) {
-            if (strcmp(arg, "--") == 1) {
-                printf("Short flag\n");
+        for (i = 0; !rh__arg_is_null(args[i]); ++i) {
+
+            // Short flag
+            if (arg[0] == '-' && arg[1] != '-') {
+                printf("Short flag: ");
+                for (ip = arg + 1; *ip != '\0'; ++ip) {
+                    printf("%c", *ip);
+                    if (*ip == args[i].shortarg) {
+                        args[i].parse(&argc, &argv, args[i].var);
+                    } // TODO: else error message (invalid flag)
+                }
+                printf("\n");
+                break;
             }
 
-            if (!strcmp(arg, inc_args->longarg) || !strcmp(arg, inc_args->shortarg)) {
-                printf("Found a match: %s\n", inc_args->longarg);
-                inc_args->parse(&argc, &argv, inc_args->var);
+            // Long flag
+            if (arg[0] == '-' && arg[1] == '-') {
+                if (strcmp(arg + 2, args[i].longarg) == 0) {
+                    printf("Long flag: %s\n", args[i].longarg);
+                    args[i].parse(&argc, &argv, args[i].var);
+                    break;
+                }
+                continue;
+            }
+
+            // Sub parser
+            if (!strcmp(arg, args[i].longarg) || (strlen(arg) == 1 && arg[0] == args[i].shortarg)) {
+                rh_args_parse(argc, argv, (RHFlag*) args[i].var);
             }
         }
     } while (argc > 0);
@@ -95,14 +137,32 @@ extern void rh_parser_bool(int *argc, char ***argv, void **var)
     *var = (void*) &rt;
 }
 
+extern void rh_action_help(int *argc, char ***argv, void **var)
+{
+    (void) argc;
+    (void) argv;
+    (void) var;
+}
 
-static char *rh_args_shift(int *argc, char ***argv)
+extern char *rh_args_shift(int *argc, char ***argv)
 {
     RH_ASSERT(*argc > 0);
     char *rt = **argv;
     *argc -= 1;
     *argv += 1;
     return rt;
+}
+
+static inline bool rh__arg_is_null(RHFlag arg)
+{
+    if (arg.longarg == NULL &&
+            arg.shortarg == 0 &&
+            arg.argtype == 0 &&
+            arg.parse == NULL &&
+            arg.var == NULL) {
+        return true;
+    }
+    return false;
 }
 
 #endif // RH_H
